5b5ea0ddf287d34bba58991111446cd789d4ed9f
Denis <82663536+Denismapll@users.noreply.github.com>
2022-09-10 07:25:23 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,22 +3,34 @@ const btn2 = document.getElementById('btn2')
 const btn3 = document.getElementById('btn3')
 const btn4 = document.getElementById('btn4')
 const btn5 = document.getElementById('btn5')
+const btn6 = document.getElementById('btn6')
+const btn7 = document.getElementById('btn7')
+const btn8 = document.getElementById('btn8')
 
 btn1.addEventListener("click", (()=>{
     window.location.href = "3 Column Previwer Card"
 }))
 btn2.addEventListener("click", (()=>{
-    window.location.href = "google.com"
+    window.location.href = "Fylo dark theme landing page master"
 }))
 btn3.addEventListener("click", (()=>{
-    window.location.href = "google.com"
+    window.location.href = "Interactive rating component main"
 }))
 btn3.addEventListener("click", (()=>{
-    window.location.href = "google.com"
+    window.location.href = "NFT Previer Card"
 }))
 btn4.addEventListener("click", (()=>{
-    window.location.href = "google.com"
+    window.location.href = "Product preview card component"
 }))
 btn5.addEventListener("click", (()=>{
+    window.location.href = "Social media dashboard with theme switcher"
+}))
+btn6.addEventListener("click", (()=>{
+    window.location.href = "Testimonials grid section main"
+}))
+btn7.addEventListener("click", (()=>{
+    window.location.href = "Time tracking dashborad main"
+}))
+btn8.addEventListener("click", (()=>{
     window.location.href = "google.com"
 }))
