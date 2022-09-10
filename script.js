f6cf1b4f5889689ccd320e2c3514637725dddf81
Denis <82663536+Denismapll@users.noreply.github.com>
2022-09-10 07:18:23 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+const btn1 = document.getElementById('btn1')
+const btn2 = document.getElementById('btn2')
+const btn3 = document.getElementById('btn3')
+const btn4 = document.getElementById('btn4')
+const btn5 = document.getElementById('btn5')
+
+btn1.addEventListener("click", (()=>{
+    window.location.href = "google.com"
+}))
+btn2.addEventListener("click", (()=>{
+    window.location.href = "google.com"
+}))
+btn3.addEventListener("click", (()=>{
+    window.location.href = "google.com"
+}))
+btn3.addEventListener("click", (()=>{
+    window.location.href = "google.com"
+}))
+btn4.addEventListener("click", (()=>{
+    window.location.href = "google.com"
+}))
+btn5.addEventListener("click", (()=>{
+    window.location.href = "google.com"
+}))
